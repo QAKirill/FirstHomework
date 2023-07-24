aecbb3282fcaba536313bd4e8f59e8bb9c31fa69
Kirill <ghostman92@ya.ru>
2023-07-24 03:02:31 +0300

    added final result check

--- src/test/java/qa/homework/FirstHomework.java
+++ src/test/java/qa/homework/FirstHomework.java
@@ -4,9 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
-import java.awt.*;
-
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -18,11 +16,10 @@ public class FirstHomework {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-
     }
 
     @Test
-    void fillFormTest() throws AWTException {
+    void fillFormTest() {
         open("/automation-practice-form");
 
         $(firstName).setValue("Kirill");
@@ -36,19 +33,26 @@ public class FirstHomework {
         $(new By.ByClassName(yearSelect)).click();
         $(byText("1992")).click();
         $(byText("18")).click();
-        $(new By.ByClassName(subjectsContainer)).click();
-
-        Robot robot = new Robot();
-        robot.keyPress(69);
-
+        $(subjectsContainer).setValue("English").pressEnter();
         $(byText("English")).click();
         $(byText(hobbiesCheckbox)).click();
         $(uploadPicture).uploadFromClasspath("128.png");
-        $(currentAddress).setValue("Ленина 1");
+        $(currentAddress).setValue("Lenina 1");
         $(selectState).click();
         $(byText("NCR")).click();
         $(selectCity).click();
         $(byText("Gurgaon")).click();
+
         $(submitButton).click();
+
+        $(new By.ByClassName("table-responsive")).shouldHave(text("Kirill Ivanov"));
+        $(new By.ByClassName("table-responsive")).shouldHave(text("my@mail.ru"));
+        $(new By.ByClassName("table-responsive")).shouldHave(text("1555232325"));
+        $(new By.ByClassName("table-responsive")).shouldHave(text("18 August,1992"));
+        $(new By.ByClassName("table-responsive")).shouldHave(text("English"));
+        $(new By.ByClassName("table-responsive")).shouldHave(text("Music"));
+        $(new By.ByClassName("table-responsive")).shouldHave(text("128.png"));
+        $(new By.ByClassName("table-responsive")).shouldHave(text("Lenina 1"));
+        $(new By.ByClassName("table-responsive")).shouldHave(text("NCR Gurgaon"));
     }
 }
--- src/test/java/qa/homework/PageObject.java
+++ src/test/java/qa/homework/PageObject.java
@@ -1,4 +1,5 @@
 package qa.homework;
+
 public class PageObject {
     public final static String firstName = "#firstName";
     public final static String lastName = "#lastName";
@@ -8,7 +9,7 @@ public class PageObject {
     public final static String dateOfBirth = "#dateOfBirthInput";
     public final static String monthSelect = "react-datepicker__month-select";
     public final static String yearSelect = "react-datepicker__year-select";
-    public final static String subjectsContainer = "css-1g6gooi";
+    public final static String subjectsContainer = "#subjectsInput";
     public final static String hobbiesCheckbox = "Music";
     public final static String uploadPicture = "input[type=file]";
     public final static String currentAddress = "#currentAddress";
