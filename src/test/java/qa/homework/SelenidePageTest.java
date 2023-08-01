d9538515e50daef393d40850403a70605172fbd9
Kirill <ghostman92@ya.ru>
2023-08-01 11:27:38 +0300

    Добавил проверку на наличие многострочных примеров кода для JUnit5

--- src/test/java/qa/homework/SelenidePageTest.java
+++ src/test/java/qa/homework/SelenidePageTest.java
@@ -31,5 +31,33 @@ public class SelenidePageTest {
         $("#wiki-tab").click();
         $("#wiki-body").$(byText("Soft assertions")).should(Condition.visible).click();
         $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
+        $("#wiki-body").shouldHave(text(
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}\n" +
+                "Or register extension inside test class:\n" +
+                "\n" +
+                "class Tests {\n" +
+                "  @RegisterExtension \n" +
+                "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
+                "\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}\n" +
+                "You can use the old good dependency \"com.codeborne:selenide\" because it contains JUnit5-specific classes."));
     }
 }
