c7d856303e88a85778fa3107455ff63753fd331a
Kirill <ghostman92@ya.ru>
2023-08-02 22:41:57 +0300

    Переименован тест

--- src/test/java/qa/homework/DragAndDropTests.java
+++ src/test/java/qa/homework/DragAndDropTests.java
@@ -1,6 +1,7 @@
 package qa.homework;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -22,7 +23,8 @@ public class DragAndDropTests {
 
         $("#column-a header").shouldHave(text("A"));
         $("#column-b header").shouldHave(text("B"));
-        $("#column-a").dragAndDropTo($("#column-b"));
+        //$("#column-a").dragAndDropTo($("#column-b")); //deprecated
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
         $("#column-a header").shouldHave(text("B"));
         $("#column-b header").shouldHave(text("A"));
         //работает
