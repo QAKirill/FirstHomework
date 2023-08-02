7d04c9057d8ac2b57225c3231bceafe493a5dbc9
Kirill <ghostman92@ya.ru>
2023-08-02 22:13:17 +0300

    Добавлен класс GithubSolutionHoverTest с заданием из п.1 Добавлен класс DragAndDropTests с заданием из п.2

---
+++ src/test/java/qa/homework/DragAndDropTests.java
@@ -0,0 +1,38 @@
+package qa.homework;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void replaceAB() {
+        open("/drag_and_drop");
+
+        $("#column-a header").shouldHave(text("A"));
+        $("#column-b header").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+        //работает
+    }
+
+    @Test
+    void replaceABusingActions() {
+        open("/drag_and_drop");
+
+        actions().dragAndDrop($("column-a"), $("column-b"));
+        //тоже работает
+    }
+}
