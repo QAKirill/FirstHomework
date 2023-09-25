846c8b3a04b796af785d5d08a8ec89cfc8fcb8f3
Kirill <ghostman92@ya.ru>
2023-09-26 00:20:58 +0300

    Добавлен prop_test

--- src/test/java/qa/homework/properties/pt.java
+++ src/test/java/qa/homework/properties/pt.java
@@ -1,14 +1,24 @@
 package qa.homework.properties;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class pt {
 
     @Test
     @Tag("prop")
     void name() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         String browser = System.getProperty("browser", "Edge");
-        System.out.println(browser);
+
+        step("Выводим:", () ->
+        System.out.println(browser));
+        Allure.addAttachment("Browser = ", browser);
     }
 }
